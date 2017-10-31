41848f24e72c434140f0770924f5e2b6dd27efaa
Jaime Rodriguez <jrodev@yahoo.es>
2017-10-31 14:20:27 +0000

    update socket

--- public/js/menu/socket.js
+++ public/js/menu/socket.js
@@ -1,27 +1,31 @@
-var socket = io.connect('https://socket-menu.herokuapp.com', { 'forceNew': true });
+var socket = io.connect('https://socket-menu.herokuapp.com', {
+    'forceNew': true
+});
 
 socket.on('messages', function(data) {
-  console.log(data);
-  render(data);
+    console.log(data);
+    render(data);
 })
 
-function render (data) {
-  var html = data.map(function(elem, index) {
-    return(`<div>
+function render(data) {
+    var html = data.map(function(elem, index) {
+        return (`<div>
               <strong>${elem.author}</strong>:
               <em>${elem.text}</em>
-            </div>`);
-  }).join(" ");
+            </div>
+    `);
+    }).join(" ");
 
-  document.getElementById('messages').innerHTML = html;
+    document.getElementById('messages').innerHTML = html;
 }
 
 function addMessage(e) {
-  var message = {
-    author: document.getElementById('username').value,
-    text: document.getElementById('texto').value
-  };
+    var message = {
+        author: document.getElementById('username').value,
+        text: document.getElementById('texto').value
+    };
 
-  socket.emit('new-message', message);
-  return false;
+    socket.emit('new-message', message);
+    
+    return false;
 }
